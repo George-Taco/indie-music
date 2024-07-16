98adb6b8b9ed538ad46d962259bdceab55fa2aad
George-Taco <saorotmg@gmail.com>
2024-07-15 21:54:01 -0400

    Cleaned up App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './firebaseConfig';
-import { getFirestore, addDoc, collection } from "firebase/firestore";
 import Home from './pages/Home';
 import ListenerLeaderboard from './pages/ListenerLeaderboard';
 import Login from './pages/Login';

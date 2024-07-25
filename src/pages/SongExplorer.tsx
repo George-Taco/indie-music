dd58c26983384c27b4b6b05196887bc31a6f787b
youseethisname <armaantheboss2007@gmail.com>
2024-07-24 20:41:43 -0400

    Styling the Embed

--- src/pages/SongExplorer.tsx
+++ src/pages/SongExplorer.tsx
@@ -1,5 +1,14 @@
-function SongExplorer() {
-    return (<h1>Song Explorer</h1>);
+import heart from '../icons/Heart.png'
+
+function SongExplorer(){
+    return (
+        <div>
+            <iframe className="leftSongs" src="https://open.spotify.com/embed/track/2BJSMvOGABRxokHKB0OI8i?utm_source=generator" width="300" height="400vh" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <button id="test">
+                <img src={heart}></img>
+            </button>
+        </div>
+     )   
 }
 
-export default SongExplorer;
+export default SongExplorer

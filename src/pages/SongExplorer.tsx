f63a4d9712b06d1013c8f0cd187975d1dc59e954
George-Taco <saorotmg@gmail.com>
2024-07-24 21:51:47 -0400

    Cleaned merge

--- src/pages/SongExplorer.tsx
+++ src/pages/SongExplorer.tsx
@@ -1,4 +1,7 @@
 import heart from '../icons/Heart.png'
+import { useState } from 'react';
+import '../firebaseConfig';
+
 
 function SongExplorer(){
     return (
@@ -10,103 +13,6 @@ function SongExplorer(){
         </div>
      )   
 
-import React, { useState } from 'react';
-import '../firebaseConfig';
-import { getFirestore, addDoc, collection, getDocs, query, DocumentData, doc, getDoc } from "firebase/firestore";
-
-
-
-function SongExplorer() {
-
-    const temporaryArr: DocumentData[] = [];
-
-    const [inputValue1, setInputValue1] = useState('');
-    const [inputValue2, setInputValue2] = useState('');
-    const [inputValue3, setInputValue3] = useState('');
-    const [inputValue4, setInputValue4] = useState('');
-    let [storedValues, setStoredValues] = useState(temporaryArr);
-  
-    const db = getFirestore();
-
-    const readDoc = async () => {
-        const docRef = doc(db, "songPreviews", "GCwzcxLSZnnYuE0XBuLx");
-        const docSnap = await getDoc(docRef);
-
-        if (docSnap.exists()) {
-        console.log("Document data:", docSnap.data());
-        } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-        }
     }
-
-    
-  
-    // save
-    const saveDataToFireStore = async () => {
-      const docRef = await addDoc(collection(db, "songPreviews"), {
-        name: inputValue1,
-        artist: inputValue2,
-        likes: inputValue3,
-        id: inputValue4
-
-      });
-      console.log("Document written to database");
-    }
-
-    const fetchDataFromFireStore = async () => {
-        const querySnapshot = await getDocs(collection(db, "songPreviews"));
-        const temporaryArr: DocumentData[] = [];
-        querySnapshot.forEach((doc) => {
-            temporaryArr.push(doc.data());
-        });
-        setStoredValues(temporaryArr);
-    }
-
-    return (
-        <div className="SongExplorer">
-            <h1>Song Explorer</h1>
-            <input
-                type='text'
-                value={inputValue1}
-                onChange={(e) => setInputValue1(e.target.value)}
-            />
-            <input
-                type='text'
-                value={inputValue2}
-                onChange={(e) => setInputValue2(e.target.value)}
-            />
-            <input
-                type='text'
-                value={inputValue3}
-                onChange={(e) => setInputValue3(e.target.value)}
-            />
-            <input
-                type='text'
-                value={inputValue4}
-                onChange={(e) => setInputValue4(e.target.value)}
-            />
-            <button onClick={saveDataToFireStore}>Save to Firestore</button> <br></br>
-
-            <button onClick={fetchDataFromFireStore}>Fetch from Firestore</button>
-
-            <iframe src="https://open.spotify.com/embed/track/0nsCBf5okWMVoaiDU9xiK7" width="250" height="450"></iframe>
-
-            <button onClick={readDoc}>Read doc from Firestore</button>
-
-            <div>
-                {storedValues.map( (item, index) => (
-                    <div key={index}>
-                        <p>{item.field1}: {item.field2}</p>
-                    </div>
-                )
-                )}
-            </div>
-
-        </div>
-
-
-    );
-}
 
 export default SongExplorer

95ed72c823e1a2d3a091eab45bb3d61f01767d42
George-Taco <saorotmg@gmail.com>
2024-08-03 00:10:11 -0400

    Connected liking functionality to firebase

--- src/components/SongEmbed.tsx
+++ src/components/SongEmbed.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import heart from '../icons/Heart.png';
 import filledHeart from '../icons/Filledheart.png';
 import { getUserId } from '../firebase/firebaseFunctions';
-import { setDoc, getFirestore, doc } from 'firebase/firestore'
+import { setDoc, getFirestore, doc, getDoc, query, where } from 'firebase/firestore'
 
 interface Props {
     trackId: string;
@@ -10,32 +10,49 @@ interface Props {
 }
 
 function SongEmbed({ trackId, numLikes }: Props) {
+
     const [isClicked, setIsClicked] = useState(false);
+    const [loadedLikedTracks, setLoadedLikedTracks] = useState(['']);
     const db = getFirestore();
     const embedUrl = `https://open.spotify.com/embed/track/${trackId}`;
     let userId = getUserId();
     let confirmedUserId: string = userId as string;
 
+    const docRef = doc(db, "LikedSongs", confirmedUserId);
+
+    const loadLikedData = async () => {
+
+        const likedTracksDoc = await getDoc(docRef);
+        const likedTracks = likedTracksDoc.get('ids');
+        return likedTracks;
+    }
+
 
     useEffect(() => {
-        const likedTracks = JSON.parse(localStorage.getItem('likedTracks') || '[]');
-        setIsClicked(likedTracks.includes(trackId));
+        //const likedTracks = JSON.parse(localStorage.getItem('likedTracks') || '[]');
+        
+        loadLikedData().then((likedTracks) => {
+            setIsClicked(likedTracks.includes(trackId));
+            setLoadedLikedTracks(likedTracks);
+        })
+        //setIsClicked(likedTracks.includes(trackId));
     }, [trackId]);
 
     
 
     const handleLikeClick = async () => {
-        const likedTracks = JSON.parse(localStorage.getItem('likedTracks') || '[]');
+        //const likedTracks = JSON.parse(localStorage.getItem('likedTracks') || '[]');
+        let likedTracks = [...loadedLikedTracks];
 
         if (isClicked) {
             const updatedTracks = likedTracks.filter((id: string) => id !== trackId);
-            localStorage.setItem('likedTracks', JSON.stringify(updatedTracks));
+            //localStorage.setItem('likedTracks', JSON.stringify(updatedTracks));
             await setDoc(doc(db, "LikedSongs", confirmedUserId), {
                 ids: updatedTracks
             });
         } else {
             likedTracks.push(trackId);
-            localStorage.setItem('likedTracks', JSON.stringify(likedTracks));
+            //localStorage.setItem('likedTracks', JSON.stringify(likedTracks));
             await setDoc(doc(db, "LikedSongs", confirmedUserId), {
                 ids: likedTracks
             });
